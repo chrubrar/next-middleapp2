a8277cd0613e36daaad20f1bcac04412c81eaf05
Charnjiv Brar <chruquantum@gmail.com>
2024-06-06 23:21:04 -0400

    fixed issue with midleware issue

--- app/middleware.ts
+++ app/middleware.ts
@@ -2,14 +2,15 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  // Log the current request pathname
-  console.log("Current path:", request.nextUrl.pathname);
+    // Log the current request pathname
+    console.log("Current path:", request.nextUrl.pathname);
     //return NextResponse.next();
     return NextResponse.redirect(new URL('/home', request.url))
+}
 
 export const config = {
-  matcher: [
-    // match all routes except static files and APIs
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
-  ],
+    matcher: [
+        // match all routes except static files and APIs
+        "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    ],
 };

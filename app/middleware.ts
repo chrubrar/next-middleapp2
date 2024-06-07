9f2b1aa5a29a8fbbd21134e0c476f6a5d95cfb01
Charnjiv Brar <chruquantum@gmail.com>
2024-06-06 23:18:13 -0400

    changed middleware.ts

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   // Log the current request pathname
   console.log("Current path:", request.nextUrl.pathname);
-  return NextResponse.next();
-}
+    //return NextResponse.next();
+    return NextResponse.redirect(new URL('/home', request.url))
 
 export const config = {
   matcher: [

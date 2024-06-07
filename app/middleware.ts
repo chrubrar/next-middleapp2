691c7427a1f8459300f8426ecfff787680f4ae7d
Charnjiv Brar <chruquantum@gmail.com>
2024-06-07 00:19:22 -0400

    Revert "added MUI theme"
    This reverts commit 7d0834a830b34f60ac47f7c7c9f42941e26165c4.
    
    Reverting bad MUI code
    git commit -m "replacing bad mui code'

--- app/middleware.ts
+++ app/middleware.ts
@@ -4,6 +4,13 @@ import type { NextRequest } from "next/server";
 export async function middleware(request: NextRequest) {
     // Log the current request pathname
     console.log("Current path:", request.nextUrl.pathname);
-    return NextResponse.next();
-    //return NextResponse.redirect(new URL('/home', request.url))
+    //return NextResponse.next();
+    return NextResponse.redirect(new URL('/home', request.url))
+}
+
+export const config = {
+    matcher: [
+        // match all routes except static files and APIs
+        "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    ],
 };

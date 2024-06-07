7d0834a830b34f60ac47f7c7c9f42941e26165c4
Charnjiv Brar <chruquantum@gmail.com>
2024-06-06 23:58:44 -0400

    added MUI theme

--- app/middleware.ts
+++ app/middleware.ts
@@ -4,13 +4,6 @@ import type { NextRequest } from "next/server";
 export async function middleware(request: NextRequest) {
     // Log the current request pathname
     console.log("Current path:", request.nextUrl.pathname);
-    //return NextResponse.next();
-    return NextResponse.redirect(new URL('/home', request.url))
-}
-
-export const config = {
-    matcher: [
-        // match all routes except static files and APIs
-        "/((?!api|_next/static|_next/image|favicon.ico).*)",
-    ],
+    return NextResponse.next();
+    //return NextResponse.redirect(new URL('/home', request.url))
 };
